a37086a7148339299bd75fd6ca2a096b9ca17808
Jeong Hun <66016555+jeonghun94@users.noreply.github.com>
2022-05-11 15:45:42 +0900

    re reset

--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from "express";
 import {
   saleCoupon,
@@ -15,21 +14,3 @@ adminRouter
 adminRouter.route("/sale-coupon/excel").get(saleCouponExcel);
 adminRouter.route("/setting-account").get(settingAccount);
 export default adminRouter;
-=======
-import express from "express";
-import {
-  saleCoupon,
-  saleCouponExcel,
-  settingAccount,
-} from "../controllers/adminController";
-import { uploadFiles } from "../middleware";
-
-const adminRouter = express.Router();
-adminRouter
-  .route("/sale-coupon")
-  .get(saleCoupon)
-  .post(uploadFiles.single("file"), saleCoupon);
-adminRouter.route("/sale-coupon/excel").get(saleCouponExcel);
-adminRouter.route("/setting-account").get(settingAccount);
-export default adminRouter;
->>>>>>> c9d84b091ea547458fa74f9e2f81790c122b25bd
