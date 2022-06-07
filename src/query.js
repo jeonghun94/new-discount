4e63d4df7b9d884b045ee1a1e01a6961f86e7d19
Jeong Hun <66016555+jeonghun94@users.noreply.github.com>
2022-06-07 11:21:16 +0900

    표출 할인권 수정

--- src/query.js
+++ src/query.js
@@ -95,12 +95,10 @@ export const DISCOUNT_QUERY = {
   SEARCH_PAY_COUPON: (obj) => {
     const opiton = obj ? `AND b.shopCode = '${obj.shopCode}'` : "";
     const nullCheck = obj.nullCheck
-      ? "AND b.used IS NULL "
-      : "AND b.used = 'Y'";
+      ? " AND b.used IS NULL "
+      : " AND b.used = 'Y'";
     const holidayCoupons =
-      obj.holidayCoupons !== null
-        ? `AND a.couponType in (${obj.holidayCoupons})`
-        : "";
+      obj.holidayCoupons !== null ? `` : `AND a.couponType in ('01', '02') `;
     const user = `SELECT a.dcName,
                           a.couponType,
                           Replace(CONVERT(VARCHAR, CONVERT(MONEY, b.stock), 1), '.00', '') AS stock
